9e64b62fb78ac5e5ad9094056258a42b8009a7c2
Gustavo <gustavo.gigante.s@gmail.com>
2019-09-07 21:56:11 +0100

    Overall style change

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <?php require_once("common/header.php"); ?>
-<div class="container">
-	<div class="row">
-		<div class="col-12" id="loginPage" style="margin-auto; text-align:center;">
-			<div class="loginContainer">
-				<img class="logo mb-4" src="images/logo.svg"/>
+<div id="loginScreen">
+	<div class="container">
+		<div class="row">
+			<div class="col-12" id="loginPage" style="margin-auto; text-align:center;">
+				<div class="loginContainer">
+					<img class="logo mb-4" src="images/logo.svg"/>
 
-				<div class="mb-3">
-					<input class="loginInputField" placeholder="UserName" id="login" type="text"></input>
-				</div>
+					<div class="mb-3">
+						<input class="loginInputField" placeholder="UserName" id="login" type="text"></input>
+					</div>
 
-				<div>
-					<input class="loginInputField" placeholder="EmailAddress" id="email" type="text"></input>
-				</div>
+					<div>
+						<input class="loginInputField" placeholder="EmailAddress" id="email" type="text"></input>
+					</div>
 
-				<div class="mt-3 text-center">
-					<button class="loginButton mr-4" id="SignUpBtn" onclick="signUpUser()">REGISTER</button>
-					<button class="loginButton" id="SingInBtn" onclick="signInUser()">LOGIN</button>
+					<div class="mt-3 text-center">
+						<button class="loginButton mr-4" id="SignUpBtn" onclick="signUpUser()">REGISTER</button>
+						<button class="loginButton" id="SingInBtn" onclick="signInUser()">LOGIN</button>
+					</div>
 				</div>
 			</div>
 		</div>
--- main.php
+++ main.php
@@ -1,141 +1,142 @@
   <?php require_once("common/header.php"); ?> 
 
   <?php require_once("topMenu/topMenu.php"); ?> 
+<div id="mainScreen">
+	<div class="container" id="mainPage">
 
-<div class="container" id="mainScreen">
+		<div class="row pt-5 position-relative">
 
-	<div class="row mt-5 position-relative">
-
-  		<div class="col-md-3 statsCard">
-  			<div class="statsInside">
-				<h3>Javascript</h3>
-				<span class="startDate d-inline-block">
-					Start: 25/08/19
-				</span>
-				<span class="currentTimeSpent d-inline-block">
-					August: 2 
-					<span>hours/week</span>
-				</span>
-				<span class="currentTimeSpent d-inline-block">
-					Average: 1
-					<span>hours/week</span>
-				</span>
-				<span class="currentTimeSpent d-inline-block">
-					Total: 10
-					<span>hours</span>
-				</span>
-				<div class="text-center">
-					<span class="d-inline-block" id="countBtn" onclick="startCounting()">
-						Start Counting
+			<div class="col-md-3 statsCard">
+				<div class="statsInside">
+					<h3>Javascript</h3>
+					<span class="startDate d-inline-block">
+						Start: 25/08/19
 					</span>
+					<span class="currentTimeSpent d-inline-block">
+						August: 2 
+						<span>hours/week</span>
+					</span>
+					<span class="currentTimeSpent d-inline-block">
+						Average: 1
+						<span>hours/week</span>
+					</span>
+					<span class="currentTimeSpent d-inline-block">
+						Total: 10
+						<span>hours</span>
+					</span>
+					<div class="text-center">
+						<span class="d-inline-block" id="countBtn" onclick="startCounting()">
+							Start Counting
+						</span>
+					</div>
 				</div>
 			</div>
-		</div>
 
 
-		<div class="col-md-3 statsCard">
-  			<div class="statsInside">
-				<h3>Javascript</h3>
-				<span class="startDate d-inline-block">
-					Start: 25/08/19
-				</span>
-				<span class="currentTimeSpent d-inline-block">
-					August: 2 
-					<span>hours/week</span>
-				</span>
-				<span class="currentTimeSpent d-inline-block">
-					Average: 1
-					<span>hours/week</span>
-				</span>
-				<span class="currentTimeSpent d-inline-block">
-					Total: 10
-					<span>hours</span>
-				</span>
-				<div class="text-center">
-					<span class="d-inline-block" id="countBtn" onclick="startCounting()">
-						Start Counting
+			<div class="col-md-3 statsCard">
+				<div class="statsInside">
+					<h3>Javascript</h3>
+					<span class="startDate d-inline-block">
+						Start: 25/08/19
 					</span>
+					<span class="currentTimeSpent d-inline-block">
+						August: 2 
+						<span>hours/week</span>
+					</span>
+					<span class="currentTimeSpent d-inline-block">
+						Average: 1
+						<span>hours/week</span>
+					</span>
+					<span class="currentTimeSpent d-inline-block">
+						Total: 10
+						<span>hours</span>
+					</span>
+					<div class="text-center">
+						<span class="d-inline-block" id="countBtn" onclick="startCounting()">
+							Start Counting
+						</span>
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="col-md-3 statsCard">
-  			<div class="statsInside">
-				<h3>Javascript</h3>
-				<span class="startDate d-inline-block">
-					Start: 25/08/19
-				</span>
-				<span class="currentTimeSpent d-inline-block">
-					August: 2 
-					<span>hours/week</span>
-				</span>
-				<span class="currentTimeSpent d-inline-block">
-					Average: 1
-					<span>hours/week</span>
-				</span>
-				<span class="currentTimeSpent d-inline-block">
-					Total: 10
-					<span>hours</span>
-				</span>
-				<div class="text-center">
-					<span class="d-inline-block" id="countBtn" onclick="startCounting()">
-						Start Counting
+			<div class="col-md-3 statsCard">
+				<div class="statsInside">
+					<h3>Javascript</h3>
+					<span class="startDate d-inline-block">
+						Start: 25/08/19
 					</span>
+					<span class="currentTimeSpent d-inline-block">
+						August: 2 
+						<span>hours/week</span>
+					</span>
+					<span class="currentTimeSpent d-inline-block">
+						Average: 1
+						<span>hours/week</span>
+					</span>
+					<span class="currentTimeSpent d-inline-block">
+						Total: 10
+						<span>hours</span>
+					</span>
+					<div class="text-center">
+						<span class="d-inline-block" id="countBtn" onclick="startCounting()">
+							Start Counting
+						</span>
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="col-md-3 statsCard">
-  			<div class="statsInside">
-				<h3>Javascript</h3>
-				<span class="startDate d-inline-block">
-					Start: 25/08/19
-				</span>
-				<span class="currentTimeSpent d-inline-block">
-					August: 2 
-					<span>hours/week</span>
-				</span>
-				<span class="currentTimeSpent d-inline-block">
-					Average: 1
-					<span>hours/week</span>
-				</span>
-				<span class="currentTimeSpent d-inline-block">
-					Total: 10
-					<span>hours</span>
-				</span>
-				<div class="text-center">
-					<span class="d-inline-block" id="countBtn" onclick="startCounting()">
-						Start Counting
+			<div class="col-md-3 statsCard">
+				<div class="statsInside">
+					<h3>Javascript</h3>
+					<span class="startDate d-inline-block">
+						Start: 25/08/19
 					</span>
+					<span class="currentTimeSpent d-inline-block">
+						August: 2 
+						<span>hours/week</span>
+					</span>
+					<span class="currentTimeSpent d-inline-block">
+						Average: 1
+						<span>hours/week</span>
+					</span>
+					<span class="currentTimeSpent d-inline-block">
+						Total: 10
+						<span>hours</span>
+					</span>
+					<div class="text-center">
+						<span class="d-inline-block" id="countBtn" onclick="startCounting()">
+							Start Counting
+						</span>
+					</div>
 				</div>
 			</div>
-		</div>
 
-	</div>
-
-	
-	<p id="showCount" style="color:white;"></p>
-	<div style="margin-auto; text-align:center;">
-			<?php require_once("Dashboard/dashboard.php"); ?>
-			<!--<canvas id="line-chart" width="500" height="100" style="width:50%; margin:auto;"></canvas>-->
 		</div>
 
 		
-	<div class="modalBg"></div>
-	<div class="modalSignOut">
-		<h3>Do you want to Logout?</h3>
-		<div>
-			<button style="float:left;" onclick="endSession()">Confirm</button>
-			<button style="float:right;" onclick="hideSignOutModal()">Cancel</button>
-		</div>
-	</div>
+		<p id="showCount" style="color:white;"></p>
+		<div style="margin-auto; text-align:center;">
+				<?php require_once("Dashboard/dashboard.php"); ?>
+				<!--<canvas id="line-chart" width="500" height="100" style="width:50%; margin:auto;"></canvas>-->
+			</div>
 
-	<div style="width:100%; height:100px;">
-		<div style = "text-align:right;">
-			<span style="font-size:30px;">User: </span>
-			<span id="loggedUser" style="font-size:20px;"></span>
+			
+		<div class="modalBg"></div>
+		<div class="modalSignOut">
+			<h3>Do you want to Logout?</h3>
+			<div>
+				<button style="float:left;" onclick="endSession()">Confirm</button>
+				<button style="float:right;" onclick="hideSignOutModal()">Cancel</button>
+			</div>
 		</div>
 
+		<div style="width:100%; height:100px;">
+			<div style = "text-align:right;">
+				<span style="font-size:30px;">User: </span>
+				<span id="loggedUser" style="font-size:20px;"></span>
+			</div>
+
+		</div>
 	</div>
 </div>
 <script>
